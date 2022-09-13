e56bd7d7f21df594caf992190557efb0f4e2693c
Luanderr21 <3470912585@qq.com>
2022-09-13 20:13:10 +0800

    modified task02 and task03

--- src/task02.java
+++ src/task02.java
@@ -1,21 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class task02 {
     public static void main(String[] args) {
         System.out.println("输入整数数组:");
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
+        //解析输入字符串到int数组
         String[] datas = s.split(",");
-        List<Integer> list = new ArrayList<>(Collections.emptyList());
-        int sum = 0;
-        for (String string : datas){
-            if (Integer.valueOf(string) >= 0){
-                sum += Integer.valueOf(string);
-            }
+        int[] nums = new int[datas.length];
+        for (int i = 0; i < datas.length; i++) {
+            nums[i] = Integer.valueOf(datas[i]);
         }
-        System.out.println("最大和为"+sum);
+        //最大子数组字典
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = Math.max(sum[i-1] + nums[i], nums[i]);
+        }
+        //排序返回字典最大值
+        Arrays.sort(sum);
+        System.out.println("最大和为"+ sum[sum.length - 1]);
     }
 }
--- src/task03.java
+++ src/task03.java
@@ -1,8 +1,21 @@
 public class task03 {
     public static void main(String[] args) {
-        for (int i = 0; i < 20000; i++) {
-
-
+        int count = 0;
+        for (int i = 1; i <= 20000; i++) {
+            boolean flag = true;
+            //从1到n遍历 优化为 2到n平方根遍历
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.print(i + " ");
+                count++;
+                if (count % 5 == 0)
+                    System.out.print('\n');
+            }
         }
     }
 }
